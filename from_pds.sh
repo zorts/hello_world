2e61a66cbaea1c21ea6f60d5eecc6a43fb511eca
Jerry Callen <jcallen@rocketsoftware.com>
2017-09-28 11:30:47 -0400

    Add some comments to the to/from PDS scripts

--- from_pds.sh
+++ from_pds.sh
@@ -1,14 +1,14 @@
 #!/bin/env bash
 
-# Copy the files
+# Copy the files. See the comments in to_pds.sh for more information.
 cp -S a=.c "//demo.c" .
 cp -S a=.h "//demo.h" .
 cp -U -S a=.md "//demo.txt" .
 
-# Fix the tags
+# Make sure that the returned files are tagged as EBCDIC (IBM-1047)
 chtag -tc1047 *.c *.h *.md
 
-# Fix the modes (this is a little dubious...)
+# Make sure that the file modes are appropriate
 chmod 644 *.c *.h *.md
 
 # See if anything changed
--- to_pds.sh
+++ to_pds.sh
@@ -1,6 +1,14 @@
 #!/bin/env bash
 
-# Copy the files
+# Copy the files. The "-s" option is z/OS-specific; it controls 
+# the way that file suffices are handled. In this case, "-S d="
+# is used to deleted the specified suffix, so that the member name
+# will just be the min part of the file name.
+#
+# For more information, see:
+#
+# https://www.ibm.com/support/knowledgecenter/SSLTBW_2.2.0/com.ibm.zos.v2r2.bpxa500/cp.htm
+
 cp -S d=.c *.c "//demo.c"
 cp -S d=.h *.h "//demo.h"
 cp -S d=.md *.md "//demo.txt"
